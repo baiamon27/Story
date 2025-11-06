31ae2aae3bec29d2dc76dbeec5d2e218dcbdc14a
baiamon27 <baiamonlangpohthmi27@gmail.com>
2025-11-06 20:18:42 +0530

    Add files via upload

--- backend/models/story.js
+++ backend/models/story.js
@@ -1,1 +1,36 @@
+const mongoose = require('mongoose');
 
+const StorySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  isPublic: {
+    type: Boolean,
+    default: false
+  },
+  tags: [String],
+  wordCount: {
+    type: Number,
+    default: 0
+  }
+}, {
+  timestamps: true
+});
+
+// Calculate word count before saving
+StorySchema.pre('save', function(next) {
+  this.wordCount = this.content.split(/\s+/).filter(word => word.length > 0).length;
+  next();
+});
+
+module.exports = mongoose.model('Story', StorySchema);
